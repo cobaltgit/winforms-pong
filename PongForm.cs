e9c79a4f968ff2080aed86e81e522439a59c6eef
cobaltgit <cobaltmail4204@gmail.com>
2023-06-25 17:33:14 +0100

    Use PascalCase for variable names

--- PongForm.cs
+++ PongForm.cs
@@ -2,15 +2,15 @@ namespace Pong
 {
     public partial class PongForm : Form
     {
-        private int xSpeed = 5;
-        private int ySpeed = 5;
+        private int XSpeed = 5;
+        private int YSpeed = 5;
 
-        private int playerOneScore = 0;
-        private int playerTwoScore = 0;
+        private int PlayerOneScore = 0;
+        private int PlayerTwoScore = 0;
 
-        private bool firstStarted = true;
-        private bool cooldown = true;
-        private bool paused = false;
+        private bool FirstStarted = true;
+        private bool Cooldown = true;
+        private bool Paused = false;
 
         private Random rng = new Random();
 
@@ -29,7 +29,7 @@ namespace Pong
 
         private void PongForm_Deactivate(object sender, EventArgs e)
         {
-            if (!paused)
+            if (!Paused)
             {
                 TogglePause();
             }
@@ -37,9 +37,9 @@ namespace Pong
 
         private void PongForm_Activated(object sender, EventArgs e)
         {
-            if (firstStarted)
+            if (FirstStarted)
             {
-                firstStarted = false;
+                FirstStarted = false;
             } 
             else 
             { 
@@ -49,51 +49,51 @@ namespace Pong
 
         private void BallTimer_Tick(object sender, EventArgs e)
         {
-            if (cooldown)
+            if (Cooldown)
             {
-                cooldown = false;
+                Cooldown = false;
                 BallTimer.Interval = 15;
             }
 
-            BallPic.Left -= xSpeed;
-            BallPic.Top -= ySpeed;
+            BallPic.Left -= XSpeed;
+            BallPic.Top -= YSpeed;
 
             if (BallPic.Left > 775)
             {
-                PlayerOneScoreLabel.Text = (++playerOneScore).ToString();
+                PlayerOneScoreLabel.Text = (++PlayerOneScore).ToString();
                 BallPic.Location = new Point(this.Width / 2, rng.Next(1, this.Height));
                 BallTimer.Interval = 1000;
                 BallPic.Hide();
-                cooldown ^= true;
+                Cooldown ^= true;
                 BallPic.Show();
-                xSpeed = -xSpeed;
-                ySpeed = -ySpeed;
+                XSpeed = -XSpeed;
+                YSpeed = -YSpeed;
             }
             else if (BallPic.Left < 0)
             {
-                PlayerTwoScoreLabel.Text = (++playerTwoScore).ToString();
+                PlayerTwoScoreLabel.Text = (++PlayerTwoScore).ToString();
                 BallPic.Location = new Point(this.Width / 2, rng.Next(1, this.Height));
                 BallTimer.Interval = 1000;
                 BallPic.Hide();
-                cooldown ^= true;
+                Cooldown ^= true;
                 BallPic.Show();
-                xSpeed = -xSpeed;
-                ySpeed = -ySpeed;
+                XSpeed = -XSpeed;
+                YSpeed = -YSpeed;
             }
 
             if (BallPic.Top < 0 || BallPic.Top > 550)
             {
-                ySpeed = -ySpeed;
+                YSpeed = -YSpeed;
             }
 
             if (BallPic.Bounds.IntersectsWith(PlayerOnePaddlePic.Bounds) || BallPic.Bounds.IntersectsWith(PlayerTwoPaddlePic.Bounds))
             { // todo: the ball shouldn't go up or down if it bounces off the middle part of the paddle pictureboxes
-                xSpeed = -xSpeed;
+                XSpeed = -XSpeed;
             }
         }
         private void PongForm_KeyDown(object sender, KeyEventArgs e) // caveat: player 1 and player 2 cannot control their paddles at the same time due to a WinForms limitation
         {
-            if (!paused)
+            if (!Paused)
             {
                 switch (e.KeyCode) // the paddles will not completely reach the top/bottom of the screen as originally featured in the real 1972 circuit board
                 {
@@ -132,8 +132,8 @@ namespace Pong
 
         private void TogglePause()
         {
-            paused ^= true;
-            if (paused)
+            Paused ^= true;
+            if (Paused)
             {
                 BallTimer.Stop();
                 this.Text = "Pong: Press ESC or refocus window to unpause";
